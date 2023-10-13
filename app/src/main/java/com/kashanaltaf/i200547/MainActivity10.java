0b23082f936403d327138aa443989dc0dfd07e0f
Kashan Altaf <kashanaltaf137@gmail.com>
2023-10-14 00:24:48 +0500

    Assignment2 Commit

--- app/src/main/java/com/kashanaltaf/i200547/MainActivity10.java
+++ app/src/main/java/com/kashanaltaf/i200547/MainActivity10.java
@@ -1,11 +1,32 @@
 package com.kashanaltaf.i200547;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.compose.material3.ProgressIndicatorDefaults;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.util.UUID;
 
 public class MainActivity10 extends AppCompatActivity {
 
@@ -15,7 +36,14 @@ public class MainActivity10 extends AppCompatActivity {
     ImageView l4;
     ImageView l5;
 
+    ImageView l6;
 
+    Uri imageUri;
+    private String myUri = "";
+
+    private StorageTask uploadTask;
+    private StorageReference storageReference;
+    private FirebaseStorage storage;
 
 
     @Override
@@ -23,11 +51,17 @@ public class MainActivity10 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main10);
 
+
         l1 = (ImageView) findViewById(R.id.lr1);
         l2 = (ImageView) findViewById(R.id.lr2);
         l3 = (ImageView) findViewById(R.id.lr3);
         l4 = (ImageView) findViewById(R.id.lr4);
         l5 = (ImageView) findViewById(R.id.lr5);
+        l6 = (ImageView) findViewById(R.id.v1);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
 
         l1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +93,16 @@ public class MainActivity10 extends AppCompatActivity {
                 Activity5();
             }
         });
+
+        l6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+            }
+        });
     }
 
     private void Activity5() {
@@ -84,5 +128,43 @@ public class MainActivity10 extends AppCompatActivity {
     private void Activity1() {
         Intent intent = new Intent(this, MainActivity5.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && requestCode==RESULT_OK && data!=null && data.getData()!=null){
+            imageUri = data.getData();
+            l6.setImageURI(imageUri);
+
+            final ProgressDialog pd = new ProgressDialog(this);
+            pd.setTitle("Uploading Image...");
+            pd.show();
+
+            final String randomKey = UUID.randomUUID().toString();
+            Uri file = Uri.fromFile(new File("C:/Users/DELL/Downloads/download.jpg"));
+            StorageReference rDef = storageReference.child("Cover Pic");
+            rDef.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    pd.dismiss();
+                    Snackbar.make(findViewById(R.id.v1), "Image Uploaded", Snackbar.LENGTH_LONG).show();
+                }
+            })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            pd.dismiss();
+                            Toast.makeText(getApplicationContext(), "Failed to Upload", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                            double progressPercent = (100.00 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            pd.setMessage("Prgress: " + (int) progressPercent + "%");
+                        }
+                    });
+        }
     }
 }

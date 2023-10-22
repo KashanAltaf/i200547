fee116b2e7b89a26ced81e16241ea211ecc31d4c
KashanAltaf <kashanaltaf137@gmail.com>
2023-10-22 18:48:07 +0500

    Assignment2 Commit

--- app/src/main/java/com/kashanaltaf/i200547/MainActivity.java
+++ app/src/main/java/com/kashanaltaf/i200547/MainActivity.java
@@ -1,8 +1,5 @@
 package com.kashanaltaf.i200547;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -12,6 +9,9 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -19,6 +19,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
     Button b1;
@@ -119,4 +120,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity2.class);
         startActivity(intent);
     }
+
+
 }
